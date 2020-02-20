de5664ff65110984ba5539341668221d265c505e
David <hakobjaniandavid@gmail.com>
2020-02-20 23:13:16 +0400

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    let result = matrix === undefined || matrix.length === 0 ? [] : TransformArray(matrix);
+
+    function TransformArray(array) {
+        for(let i = 1; i < array.length; i = i+2){
+            array[i].reverse();
+        }
+        const matrixString = array.toString();
+
+        return matrixString.split(',').map((el) => {
+          return parseInt(el, 10);
+        });
+    }
+
+    return result;
+};
